90927273cad61fb17764faed0bb34d7fb3413d84
kilokid <arlomov007@gmail.com>
2021-09-17 14:04:47 +0300

    finish homework 2

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 25000; // доход за месяц
+let income = 'Фриланс'; // дополнительный доход
+let addExpenses = 'интернет, такси, гаджеты, подписки'; // доп расходы
+let deposit = true; 
+let mission = 150000; // сумму, которую хочу накопить
+let period = 8; 
+let budgetDay = money / 30; // дневной бюджет 
 
-alert('Привет, мир!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('Hello, world!');
+console.log(addExpenses.length);
+
+console.log(`Период равен ${period} месяцев. Цель заработать ${mission} рублей.`);
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log(budgetDay);
