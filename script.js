48a5abc25648e917ebd4402f1057e12fd38bcbdc
kilokid <arlomov007@gmail.com>
2021-09-30 01:00:13 +0300

    feat: create created variables for all input fields

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const btnCalculate = document.getElementById('start');
+const btnPlus1 = document.getElementsByTagName('button')[0];
+const btnPlus2 = document.getElementsByTagName('button')[1];
+const checkbox = document.querySelector('#deposit-chek');
+const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const budgetDayValue = document.getElementsByClassName('budget_day-value');
+const expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+const incomePeriodValue = document.getElementsByClassName('income_period-value');
+const targetMonthValue = document.getElementsByClassName('target_month-value');
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelector('.income-title');
+const incomeAmount = document.querySelector('.income-amount');
+const expensesTitle = document.querySelector('.expenses-title');
+const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+const targetAmount = document.querySelector('.target-amount');
+const periodSelect = document.querySelector('.period-select');
+const budgetMonthValue = document.getElementsByClassName('budget_month-value');
+
 const isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
