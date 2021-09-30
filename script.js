0495be62367b3158e73ca142cb15549c3927838e
kilokid <arlomov007@gmail.com>
2021-09-30 22:17:26 +0300

    fix: structure & name + add periodAmount

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
 'use strict';
 
-const calculateBtn = document.getElementById('start');
-console.log(calculateBtn);
-const additionalIncomeplus = document.getElementsByTagName('button')[0];
-console.log(additionalIncomeplus);
-const complusoryExpensesPlus = document.getElementsByTagName('button')[1];
-console.log(complusoryExpensesPlus);
+const additionalIncomeButton = document.getElementsByTagName('button')[0];
+const complusoryExpensesButton = document.getElementsByTagName('button')[1];
 const depositCheckbox = document.querySelector('#deposit-check');
-console.log(depositCheckbox);
 const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
-console.log(additionalIncomeItem);
-const budgetDayValue = document.getElementsByClassName('budget_day-value')[0];
-console.log(budgetDayValue);
-const expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
-console.log(expensesMonthValue);
-const additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0];
-console.log(additionalIncomeValue);
-const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0];
-console.log(additionalExpensesValue);
-const incomePeriodValue = document.getElementsByClassName('income_period-value')[0];
-console.log(incomePeriodValue);
-const targetMonthValue = document.getElementsByClassName('target_month-value')[0];
-console.log(targetMonthValue);
-const salaryAmount = document.querySelector('.salary-amount');
-console.log(salaryAmount);
-const incomeTitle = document.querySelector('.income-items').children[0];
-console.log(incomeTitle);
-const incomeAmount = document.querySelector('.income-amount');
-console.log(incomeAmount);
-const expensesTitle = document.querySelector('.expenses-items').children[0];
-console.log(expensesTitle);
-const expensesAmount = document.querySelector('.expenses-amount');
-console.log(expensesAmount);
-const additionalExpensesItem = document.querySelector('.additional_expenses-item');
-console.log(additionalExpensesItem);
-const targetAmount = document.querySelector('.target-amount');
-console.log(targetAmount);
-const periodSelect = document.querySelector('.period-select');
-console.log(periodSelect);
+
+// right
 const budgetMonthValue = document.getElementsByClassName('budget_month-value')[0];
+const budgetDayValue = document.getElementsByClassName('budget_day-value')[0];
+const expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0];
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0];
+const incomePeriodValue = document.getElementsByClassName('income_period-value')[0];
+const targetMonthValue = document.getElementsByClassName('target_month-value')[0];
+const calculateBtn = document.getElementById('start');
+
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelector('.income-items').children[0];
+const incomeAmount = document.querySelector('.income-amount');
+const expensesTitle = document.querySelector('.expenses-items').children[0];
+const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+const targetAmount = document.querySelector('.target-amount');
+const periodSelect = document.querySelector('.period-select');
+const periodAmount = document.querySelector('.period-amount');
+
+console.log(additionalIncomeButton);
+console.log(complusoryExpensesButton);
+console.log(depositCheckbox);
+console.log(additionalIncomeItem);
+
 console.log(budgetMonthValue);
+console.log(budgetDayValue);
+console.log(expensesMonthValue);
+console.log(additionalIncomeValue);
+console.log(additionalExpensesValue);
+console.log(incomePeriodValue);
+console.log(targetMonthValue);
+console.log(calculateBtn);
+
+console.log(salaryAmount);
+console.log(incomeTitle);
+console.log(incomeAmount);
+console.log(expensesTitle);
+console.log(expensesAmount);
+console.log(additionalExpensesItem);
+console.log(targetAmount);
+console.log(periodSelect);
+console.log(periodAmount);
