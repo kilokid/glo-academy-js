b5627d7cc680c9c0548adae67130bf5d61992b3d
kilokid <arlomov007@gmail.com>
2021-09-30 23:03:14 +0300

    fear: add deposit amount & percent

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const additionalIncomeButton = document.getElementsByTagName('button')[0];
-const complusoryExpensesButton = document.getElementsByTagName('button')[1];
+const ExpensesAddButton = document.getElementsByTagName('button')[1];
 const depositCheckbox = document.querySelector('#deposit-check');
-const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const additionalIncomeItems = document.querySelectorAll('.additional_income-item');
 
 // right
 const budgetMonthValue = document.getElementsByClassName('budget_month-value')[0];
@@ -22,13 +22,15 @@ const expensesTitle = document.querySelector('.expenses-items').children[0];
 const expensesAmount = document.querySelector('.expenses-amount');
 const additionalExpensesItem = document.querySelector('.additional_expenses-item');
 const targetAmount = document.querySelector('.target-amount');
-const periodSelect = document.querySelector('.period-select');
+const periodRange = document.querySelector('.period-select');
 const periodAmount = document.querySelector('.period-amount');
+const depositAmount = document.querySelector('.deposit-amount');
+const depositPercent = document.querySelector('.deposit-percent');
 
 console.log(additionalIncomeButton);
-console.log(complusoryExpensesButton);
+console.log(ExpensesAddButton);
 console.log(depositCheckbox);
-console.log(additionalIncomeItem);
+console.log(additionalIncomeItems);
 
 console.log(budgetMonthValue);
 console.log(budgetDayValue);
@@ -46,5 +48,5 @@ console.log(expensesTitle);
 console.log(expensesAmount);
 console.log(additionalExpensesItem);
 console.log(targetAmount);
-console.log(periodSelect);
+console.log(periodRange);
 console.log(periodAmount);
