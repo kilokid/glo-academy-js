3196fe674c86df78f2188e19025c274ae5ba74ea
kilokid <arlomov007@gmail.com>
2021-10-01 17:06:55 +0300

    fix: bgimage & variable name

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-const body = document.querySelector('body');
 const booksContainer = document.querySelector('.books');
 const books = document.querySelectorAll('.book');
 const booksTitle = document.querySelectorAll('a');
 const ads = document.querySelector('.adv');
-const liBookVisibilitu = books[0].querySelectorAll('li');
-const liBookAsync = books[5].querySelectorAll('li');
-const liBookES6 = books[2].querySelectorAll('li');
+const visibilityBookElems = books[0].querySelectorAll('li');
+const asyncBookElems = books[5].querySelectorAll('li');
+const es6BookElems = books[2].querySelectorAll('li');
 
 booksContainer.prepend(books[1], books[0], books[4], books[3], books[5], books[2]);
 
-body.style.backgroundImage = 'url(./image/you-dont-know.jpg)';
+document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 
 booksTitle[4].textContent = 'Книга 3. this и Прототипы Объектов';
 
 ads.remove();
 
-liBookVisibilitu[1].after(liBookVisibilitu[3], liBookVisibilitu[6], liBookVisibilitu[8]);
-liBookVisibilitu[9].after(liBookVisibilitu[2]);
+visibilityBookElems[1].after(visibilityBookElems[3], visibilityBookElems[6], visibilityBookElems[8]);
+visibilityBookElems[9].after(visibilityBookElems[2]);
 
-liBookAsync[1].after(liBookAsync[9], liBookAsync[3], liBookAsync[4]);
-liBookAsync[7].after(liBookAsync[5]);
+asyncBookElems[1].after(asyncBookElems[9], asyncBookElems[3], asyncBookElems[4]);
+asyncBookElems[7].after(asyncBookElems[5]);
 
-liBookES6[8].insertAdjacentHTML('beforeend', '<li>Глава 8: За пределами ES6</li>');
+es6BookElems[8].insertAdjacentHTML('beforeend', '<li>Глава 8: За пределами ES6</li>');
