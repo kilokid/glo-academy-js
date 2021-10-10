d9c8e924792e78ef32a4f52115f80093afebde14
kilokid <arlomov007@gmail.com>
2021-10-11 00:15:37 +0300

    finish homework part 1

---
+++ script.js
@@ -0,0 +1,35 @@
+function DomElement(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
+
+DomElement.prototype.createElem = function() {
+    if (this.selector.charAt(0) === '.') {
+        const div = document.createElement('div');
+        div.classList.add(this.selector.slice(1));
+        div.textContent = 'Hello, world!';
+        div.style.cssText = `width: ${this.width};
+            height: ${this.height};
+            background-color: ${this.bg};
+            font-size: ${this.fontSize};`;
+        document.body.prepend(div);
+    } else if (this.selector.charAt(0) === '#') {
+        const p = document.createElement('p');
+        p.id = this.selector.slice(1);
+        p.textContent = 'Hello, world!';
+        p.style.cssText = `width: ${this.width};
+            height: ${this.height};
+            background-color: ${this.bg};
+            font-size: ${this.fontSize};`;
+        document.body.prepend(p);
+    }
+};
+
+let elem1 = new DomElement('.test', '100px', '100px', 'lightblue', '34px');
+let elem2 = new DomElement('#test2', '50px', '50px', 'red', '14px');
+
+elem1.createElem();
+elem2.createElem();
