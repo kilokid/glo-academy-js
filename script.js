d3e00f43279a7af13a72ffbf79eab3bfa9234ad7
kilokid <arlomov007@gmail.com>
2021-10-12 13:57:09 +0300

    fix: func arg

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-class DomElement {
-    constructor(selector, height, width, bg, fontSize) {
-        this.selector = selector;
-        this.height = height;
-        this.width = width;
-        this.bg = bg;
-        this.fontSize = fontSize;
-    }
+// class DomElement {
+//     constructor(selector, height, width, bg, fontSize) {
+//         this.selector = selector;
+//         this.height = height;
+//         this.width = width;
+//         this.bg = bg;
+//         this.fontSize = fontSize;
+//     }
 
-    createElem() {
-        if (this.selector.charAt(0) === '.') {
-            const div = document.createElement('div');
-            div.classList.add(this.selector.slice(1));
-            div.textContent = 'Hello, world!';
-            div.style.cssText = `width: ${this.width};
-                height: ${this.height};
-                background-color: ${this.bg};
-                font-size: ${this.fontSize};`;
-            document.body.append(div);
-        } else if (this.selector.charAt(0) === '#') {
-            const p = document.createElement('p');
-            p.id = this.selector.slice(1);
-            p.textContent = 'Hello, world!';
-            p.style.cssText = `width: ${this.width};
-                height: ${this.height};
-                background-color: ${this.bg};
-                font-size: ${this.fontSize};`;
-            document.body.append(p);
-        }
-    }
-}
+//     createElem() {
+//         if (this.selector.charAt(0) === '.') {
+//             const div = document.createElement('div');
+//             div.classList.add(this.selector.slice(1));
+//             div.textContent = 'Hello, world!';
+//             div.style.cssText = `width: ${this.width};
+//                 height: ${this.height};
+//                 background-color: ${this.bg};
+//                 font-size: ${this.fontSize};`;
+//             document.body.append(div);
+//         } else if (this.selector.charAt(0) === '#') {
+//             const p = document.createElement('p');
+//             p.id = this.selector.slice(1);
+//             p.textContent = 'Hello, world!';
+//             p.style.cssText = `width: ${this.width};
+//                 height: ${this.height};
+//                 background-color: ${this.bg};
+//                 font-size: ${this.fontSize};`;
+//             document.body.append(p);
+//         }
+//     }
+// }
 
-let elem1 = new DomElement('.test', '100px', '100px', 'lightblue', '34px').createElem();
-let elem2 = new DomElement('#test2', '50px', '50px', 'red', '14px').createElem();
+// let elem1 = new DomElement('.test', '100px', '100px', 'lightblue', '34px').createElem();
+// let elem2 = new DomElement('#test2', '50px', '50px', 'red', '14px').createElem();
+
+const DomElement = function(options) {
+    options = options || {};
+    this.selector = options.selector;
+    this.height = options.height;
+    this.width = options.width;
+    this.bg = options.bg;
+    this.fontSize = options.fontSize;
+};
+
+DomElement.prototype.create = function() {
+    const elemStyles = (tag) => {
+        tag.style.cssText = `width: ${this.width};
+            height: ${this.height};
+            background-color: ${this.bg};
+            font-size: ${this.fontSize};`;
+    };
+
+    
+};
+
+const elem1 = new DomElement().create();
