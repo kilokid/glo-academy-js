50cc7aadd056d6e50bd73ac32e8b52f67c3a6191
kilokid <arlomov007@gmail.com>
2021-10-12 15:47:03 +0300

    fix: all

--- script.js
+++ script.js
@@ -33,24 +33,40 @@
 // let elem1 = new DomElement('.test', '100px', '100px', 'lightblue', '34px').createElem();
 // let elem2 = new DomElement('#test2', '50px', '50px', 'red', '14px').createElem();
 
-const DomElement = function(options) {
-    options = options || {};
-    this.selector = options.selector;
-    this.height = options.height;
-    this.width = options.width;
-    this.bg = options.bg;
-    this.fontSize = options.fontSize;
+const DomElement = function(selector, styles) {
+    this.selector = selector;
+
+    styles = styles || {};
+    this.height = styles.height;
+    this.width = styles.width;
+    this.bg = styles.bg;
+    this.fontSize = styles.fontSize;
 };
 
 DomElement.prototype.create = function() {
-    const elemStyles = (tag) => {
-        tag.style.cssText = `width: ${this.width};
-            height: ${this.height};
-            background-color: ${this.bg};
-            font-size: ${this.fontSize};`;
-    };
+    let element;
 
+    switch (this.selector[0]) {
+        case '.':
+            element = document.createElement('div');
+            element.classList.add(this.selector.slice(1));
+            break;
+
+        case '#':
+            element = document.createElement('p');
+            element.id = this.selector.slice(1);
+            break;
+    }
+    element.textContent = 'hello world';
+
+    element.style.cssText = `width: ${this.width};
+    height: ${this.height};
+    background-color: ${this.bg};
+    font-size: ${this.fontSize};`;
+
+    document.body.append(element);
     
 };
 
-const elem1 = new DomElement().create();
+const elem1 = new DomElement('.lolwhat', {bg: 'red', width: '50px'}).create();
+const elem2 = new DomElement('#wowtest', {width: '100px', height: '100px', fontSize: '42px', bg: 'yellow'}).create();
