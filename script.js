1d30eb58bca80d278ce7fe1f069dca244044d3cd
kilokid <arlomov007@gmail.com>
2021-11-24 20:25:01 +0300

    feat: move box using arrows

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
-class DomElement {
-    constructor(selector, height, width, bg, fontSize, position) {
+document.addEventListener('DOMContentLoaded', () => {
+    const DomElement = function(selector, styles) {
         this.selector = selector;
-        this.height = height;
-        this.width = width;
-        this.bg = bg;
-        this.fontSize = fontSize;
-        this.position = position;
-    }
-
-    createElem() {
-        if (this.selector.charAt(0) === '.') {
-            const div = document.createElement('div');
-            div.classList.add(this.selector.slice(1));
-            div.textContent = 'Hello, world!';
-            div.style.cssText = `width: ${this.width};
-                height: ${this.height};
-                background-color: ${this.bg};
-                font-size: ${this.fontSize};
-                position: ${this.position}`;
-            document.body.append(div);
-        } else if (this.selector.charAt(0) === '#') {
-            const p = document.createElement('p');
-            p.id = this.selector.slice(1);
-            p.textContent = 'Hello, world!';
-            p.style.cssText = `width: ${this.width};
-                height: ${this.height};
-                background-color: ${this.bg};
-                font-size: ${this.fontSize};
-                position: ${this.position}`;
-            document.body.append(p);
+    
+        styles = styles ?? {};
+        this.height = styles.height;
+        this.width = styles.width;
+        this.bg = styles.bg;
+        this.fontSize = styles.fontSize;
+        this.position = styles.position;
+    };
+    
+    DomElement.prototype.create = function() {
+        let element;
+    
+        switch (this.selector[0]) {
+            case '.':
+                element = document.createElement('div');
+                element.classList.add(this.selector.slice(1));
+                break;
+    
+            case '#':
+                element = document.createElement('p');
+                element.id = this.selector.slice(1);
+                break;
+            
+            default:
+                element = document.createElement('div');
         }
-    }
-}
+        element.textContent = '';
+    
+        element.style.cssText = `width: ${this.width};
+        height: ${this.height};
+        background-color: ${this.bg};
+        font-size: ${this.fontSize};
+        position: ${this.position}`;
+    
+        document.body.append(element);
+        
+    };
 
-const square = new DomElement('.square', '100px', '100px', 'yellow', '18px', 'absolute').createElem();
+    const box = new DomElement('.box', {bg: 'red', width: '100px', height: '100px', position: 'absolute'}).create();
+    
+    const move = (elem) => {
+        const box = document.querySelector(elem);
+        let left = 0;
+        let top = 0;
+        
+        window.addEventListener('keydown', (event) => {
+            if (event.keyCode === 37) {
+                box.style.left = `${left -= 10}px`;
+            } else if (event.keyCode === 38) {
+                box.style.top = `${top -= 10}px`;
+            } else if (event.keyCode === 39) {
+                box.style.left = `${left += 10}px`;
+            } else if (event.keyCode === 40) {
+                box.style.top = `${top += 10}px`;
+            }
+        });
+    };
+
+    move('.box');
+});
